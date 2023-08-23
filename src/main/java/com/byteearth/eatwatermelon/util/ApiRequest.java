c9073115702db888f44462cab1f166ba95f7c366
GJunC <gaojuncheng@ifun.com>
2023-08-23 16:21:51 +0800

    调整部分实现

--- src/main/java/com/byteearth/eatwatermelon/util/ApiRequest.java
+++ src/main/java/com/byteearth/eatwatermelon/util/ApiRequest.java
@@ -4,6 +4,8 @@ import cn.hutool.http.HttpStatus;
 import com.alibaba.fastjson.JSON;
 import com.byteearth.eatwatermelon.model.entity.JsonRootBean;
 import com.byteearth.eatwatermelon.model.entity.Replies;
+import com.google.common.collect.Lists;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.IOException;
@@ -13,8 +15,9 @@ import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Date;
+import java.util.List;
 
+//@Slf4j
 public class ApiRequest {
 
     public static String request(String url) throws IOException {
@@ -40,13 +43,29 @@ public class ApiRequest {
     }
 
     public static void main(String[] args) throws IOException {
-        String url = "https://api.bilibili.com/x/v2/reply/main?jsonp=jsonp&type=1&oid=956733745&mode=3&plat=1&next=1";
-        String response = request(url);
-        JsonRootBean json = JSON.parseObject(response, JsonRootBean.class);
-        for (Replies repliesObj : json.getData().getReplies()) {
-            System.out.println("用户名称:" + repliesObj.getMember().getUname() + ",回复内容：" + repliesObj.getContent().getMessage()
-                    + "\n点赞数:" + repliesObj.getLike() + ",评论数:" + repliesObj.getRcount()
-                    + ",评论时间:" + DateUtils.format(new Date(Long.parseLong(repliesObj.getCtime() + "000")), DateUtils.DATE_TIME_CHINESE));
+        // 设置目标点赞视频id
+        String oid = "956733745";
+        // 设置目标点赞数,-1不限制,其他设置低于该点赞数的不拉取
+        int likecount = -1;
+        String url = "https://api.bilibili.com/x/v2/reply/main?jsonp=jsonp&type=1&oid=" + oid + "&mode=3&plat=1&next=1";
+
+        List<Replies> replyList = Lists.newArrayList();
+        int count = 0;
+        while (true) {
+            String response = request(url);
+            JsonRootBean json = JSON.parseObject(response, JsonRootBean.class);
+            replyList.addAll(json.getData().getReplies());
+            if (CollectionUtils.isEmpty(json.getData().getReplies())) {
+                break;
+            }
+//            log.info("{}",++count);
+            System.out.println(++count);
         }
+        System.out.println(replyList.size());
+//        for (Replies repliesObj : replyList) {
+//            System.out.println("用户名称:" + repliesObj.getMember().getUname() + ",回复内容：" + repliesObj.getContent().getMessage()
+//                    + "\n点赞数:" + repliesObj.getLike() + ",评论数:" + repliesObj.getRcount()
+//                    + ",评论时间:" + DateUtils.format(new Date(Long.parseLong(repliesObj.getCtime() + "000")), DateUtils.DATE_TIME_CHINESE));
+//        }
     }
 }
